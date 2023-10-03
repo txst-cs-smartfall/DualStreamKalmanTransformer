e5306a9f67436ffc2d7ec273b82a9316a02744c7
Tousif <tousifulhaque@iut-dhaka.edu>
2023-10-03 17:21:45 -0500

    Minor Changes

--- train.sh
+++ train.sh
@@ -4,5 +4,6 @@
 #python3 main.py --config ./config/utd/student.yaml --work-dir exps/UTD_woKD/MM --device 0 --base-lr 2.5e-2 
 
 #Utd teacher 
-python3 main.py --config ./config/utd/teacher.yaml --work-dir exps/UTD_woKD/MM --device 0 --base-lr 2.5e-3 --include-val False
-#python3 main.py --config ./config/utd/teacher.yaml --weights exps/UTD_woKD/MM/test.pt --device 0 --phase 'test'
+python3 main.py --config ./config/utd/teacher.yaml --work-dir exps/UTD_woKD/MMNorm --device 0 --base-lr 2.5e-3 --include-val True
+#python3 main.py --config ./config/utd/teacher.yaml --weights exps/UTD_woKD/MMNorm/no_kd_aug.pt --device 0 --phase 'test'
+#python3 main.py --config ./config/utd/teacher.yaml --weights exps/Sfmm_woKD/MM/no_kd_aug.pt --device 0 --phase 'test'
