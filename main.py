b86edbc68b434ef4a85b597d2fa8371dfb6d8f56
Tousif <tousifulhaque@iut-dhaka.edu>
2023-11-01 18:53:24 -0500

    Enhance: Dynamic Import of Loss function

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ from torchsummary import summary
 
 #local import 
 from Feeder.augmentation import TSFilpper
-from utils.dataprocessing import utd_processing , normalization
+from utils.dataprocessing import utd_processing , bmhad_processing,normalization
 
 def get_args():
 
     parser = argparse.ArgumentParser(description = 'Distillation')
     parser.add_argument('--config' , default = './config/utd/student.yaml')
-
+    parser.add_argument('--dataset', type = str, default= 'utd' )
     #training
     parser.add_argument('--batch-size', type = int, default = 16, metavar = 'N',
                         help = 'input batch size for training (default: 8)')
@@ -54,6 +54,11 @@ def get_args():
     parser.add_argument('--model-args', default= str, help = 'A dictionary for model args')
     parser.add_argument('--weights', type = str, help = 'Location of weight file')
     parser.add_argument('--model-saved-name', type = str, help = 'Weigt name', default='test')
+
+    #loss args
+    parser.add_argument('--loss', default='loss.BCE' , help = 'Name of loss function to use' )
+    parser.add_argument('--loss-args', default ="{}", type = str,  help = 'A dictionary for loss')
+    # parser.add_argument('--loss-args', default=str, help = 'A dictonary for loss args' )
 
     #dataloader 
     parser.add_argument('--feeder', default= None , help = 'Dataloader location')
@@ -114,7 +119,8 @@ class Trainer():
     def __init__(self, arg):
         self.arg = arg
         # self.save_arg()
-        self.load_model()
+        self.load_model(arg.model, arg.model_args)
+        self.load_loss()
         self.load_optimizer()
         self.load_data()
         self.include_val = arg.include_val
@@ -123,14 +129,25 @@ class Trainer():
 
         if self.arg.phase == 'test':
             self.load_weights(self.arg.weights)
+        
+        num_params = self.count_parameters(self.model)
+        self.print_log(f'# Parameters: {num_params}')
+
+    def count_parameters(self, model):
+                return sum(p.numel() for p in model.parameters() if p.requires_grad)
+            
 
     
-    def load_model(self):
+    def load_model(self, model, model_args):
         use_cuda = torch.cuda.is_available()
         self.output_device = self.arg.device[0] if type(self.arg.device) is list else self.arg.device
-        Model = import_class(self.arg.model)
-        self.model = Model(**self.arg.model_args).to(f'cuda:{self.output_device}' if use_cuda else 'cpu')
-        self.criterion= nn.CrossEntropyLoss().cuda(self.output_device)
+        Model = import_class(model)
+        self.model = Model(**model_args).to(f'cuda:{self.output_device}' if use_cuda else 'cpu')
+    
+    def load_loss(self):
+        criterion= import_class(self.arg.loss)
+        #loss_args = yaml.safe_load(arg.loss_args)
+        self.criterion = criterion()
     
     def load_weights(self, weights):
         self.model.load_state_dict(torch.load(self.arg.weights))
@@ -158,8 +175,19 @@ class Trainer():
         ## need to change it to dynamic import 
         self.data_loader = dict()
         if self.arg.phase == 'train':
-            train_data =  utd_processing(mode = arg.phase)
-            norm_train, acc_scaler, skl_scaler =  normalization(data = train_data, mode = 'fit')
+            if self.arg.dataset == 'utd':
+                train_data =  utd_processing(mode = self.arg.phase)
+                norm_train, acc_scaler, skl_scaler =  normalization(data = train_data, mode = 'fit')
+                val_data =  utd_processing(mode = 'val')
+                norm_val, _, _ =  normalization(data = val_data,acc_scaler=acc_scaler,
+                                               skl_scaler=skl_scaler, mode = 'transform')
+            else:
+                #train_data = bmhad_processing(mode = arg.phase)
+                train_data = np.load('data/berkley_mhad/bhmad_sliding_stride10_train.npz')
+                norm_train, acc_scaler, skl_scaler = normalization(data= train_data, mode = 'fit' )
+                #val_data  = bmhad_processing(mode = 'val')
+                val_data = np.load('data/berkley_mhad/bhmad_sliding_stride10_val.npz')
+                norm_val, _, _ = normalization(data = val_data , mode = 'transform')
             self.acc_scaler = acc_scaler
             self.skl_scaler = skl_scaler
             self.data_loader['train'] = torch.utils.data.DataLoader(
@@ -168,17 +196,21 @@ class Trainer():
                 shuffle=True,
                 num_workers=self.arg.num_worker)
             
-            val_data =  utd_processing(mode = 'val')
-            norm_val, _, _ =  normalization(data = val_data,acc_scaler=self.acc_scaler,
-                                               skl_scaler=self.skl_scaler, mode = 'transform')
+            
             self.data_loader['val'] = torch.utils.data.DataLoader(
                 dataset=Feeder(**self.arg.val_feeder_args, dataset = norm_val),
                 batch_size=self.arg.batch_size,
                 shuffle=True,
                 num_workers=self.arg.num_worker)
         else:
+            if self.arg.dataset == 'utd':
+                test_data =  utd_processing(mode = 'test')
+            else:
+                test_data = np.load('data/berkley_mhad/bhmad_sliding_stride10_test.npz')
+
+            norm_test, _, _ =  normalization(data = test_data, mode = 'fit')
             self.data_loader['test'] = torch.utils.data.DataLoader(
-                dataset=Feeder(**self.arg.test_feeder_args),
+                dataset=Feeder(**self.arg.test_feeder_args, dataset = norm_test),
                 batch_size=self.arg.test_batch_size,
                 shuffle=False,
                 num_workers=self.arg.num_worker)
@@ -196,7 +228,7 @@ class Trainer():
 
     def print_log(self, string, print_time = True):
         print(string)
-        if arg.print_log:
+        if self.arg.print_log:
             with open('{}/log.txt'.format(self.arg.work_dir), 'a') as f:
                 print(string, file = f)
 
@@ -224,10 +256,15 @@ class Trainer():
 
             # Ascent Step
             #print("labels: ",targets)
-            out, logits,predictions = self.model(acc_data.float(), skl_data.float())
+            masks, logits,predictions = self.model(acc_data.float(), skl_data.float())
             #logits = self.model(acc_data.float(), skl_data.float())
             #print("predictions: ",torch.argmax(predictions, 1) )
-            loss = self.criterion(logits, targets)
+            # bce_loss = self.criterion(logits, targets)
+            # slim_loss = 0
+            # for mask in masks: 
+            #     slim_loss += sum([self.slim_penalty(m) for m in mask])
+            # loss = bce_loss + (0.3*slim_loss)
+            loss = self.criterion(masks, logits, targets)
             loss.mean().backward()
             self.optimizer.step()
 
@@ -255,7 +292,7 @@ class Trainer():
                     self.best_accuracy = accuracy
                     state_dict = self.model.state_dict()
                     #weights = OrderedDict([[k.split('module.')[-1], v.cpu()] for k, v in state_dict.items()])
-                    torch.save(state_dict, self.arg.work_dir + '/' + self.arg.model_saved_name+ '.pt')
+                    torch.save(state_dict, self.arg.work_dir + '/' + self.arg.model_saved_name+ str(epoch)+ '.pt')
                     self.print_log('Weights Saved') 
         
         else: 
@@ -292,9 +329,14 @@ class Trainer():
                 targets = targets.cuda(self.output_device)
 
                 #_,logits,predictions = self.model(inputs.float())
-                _,logits,predictions = self.model(acc_data.float(), skl_data.float())
+                masks,logits,predictions = self.model(acc_data.float(), skl_data.float())
                 #logits = self.model(acc_data.float(), skl_data.float())
-                batch_loss = self.criterion(logits, targets)
+                # bce_loss = self.criterion(logits, targets)
+                # slim_loss = 0
+                # for mask in masks: 
+                #     slim_loss += sum([self.slim_penalty(m) for m in mask])
+                # batch_loss = bce_loss + (0.3*slim_loss)
+                batch_loss = self.criterion(masks, logits, targets)
                 loss += batch_loss.sum().item()
                 # accuracy += (torch.argmax(predictions, 1) == targets).sum().item()
                 # pred_list.extend(torch.argmax(predictions ,1).tolist())
@@ -311,7 +353,7 @@ class Trainer():
             for i, x in enumerate(predict):
                 f_r.write(str(x) +  '==>' + str(true[i]) + '\n')
         
-        self.print_log('\tValidation Loss: {:4f}. Validaiton Acc: {:2f}%'.format(loss, accuracy))
+        self.print_log('{} Loss: {:4f}. {} Acc: {:2f}%'.format(loader_name.capitalize(),loss,loader_name.capitalize(), accuracy))
         if self.arg.phase == 'train':
             if accuracy > self.best_accuracy :
                     self.best_accuracy = accuracy
@@ -321,25 +363,18 @@ class Trainer():
                     self.print_log('Weights Saved')        
 
     def start(self):
-        model_args = self.arg.model_args
-        summary(self.model,[(model_args['acc_frames'],6), (model_args['mocap_frames'], model_args['num_joints'],3)] , dtypes=[torch.float, torch.float] )
+        #summary(self.model,[(model_args['acc_frames'],3), (model_args['mocap_frames'], model_args['num_joints'],3)] , dtypes=[torch.float, torch.float] )
         if self.arg.phase == 'train':
             self.best_accuracy  = 0
             self.print_log('Parameters: \n{}\n'.format(str(vars(self.arg))))
             self.global_step = self.arg.start_epoch * len(self.data_loader['train']) / self.arg.batch_size
-
-            def count_parameters(model):
-                return sum(p.numel() for p in model.parameters() if p.requires_grad)
-            
-            num_params = count_parameters(self.model)
-            self.print_log(f'# Parameters: {num_params}')
             for epoch in range(self.arg.start_epoch, self.arg.num_epoch):
                 self.train(epoch)
                 
             self.print_log(f'Best accuracy: {self.best_accuracy}')
-            # self.print_log(f'Epoch number: {self.best_acc_epoch}')
+            #self.print_log(f'Epoch number: {self.best_acc_epoch}')
             self.print_log(f'Model name: {self.arg.work_dir}')
-            self.print_log(f'Model total number of params: {num_params}')
+            #self.print_log(f'Model total number of params: {num_params}')
             self.print_log(f'Weight decay: {self.arg.weight_decay}')
             self.print_log(f'Base LR: {self.arg.base_lr}')
             self.print_log(f'Batch Size: {self.arg.batch_size}')
